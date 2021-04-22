c7d0c04df3d96187b2fb36ff77545e6a2122749e
Rose <rosebatoeva@gmail.com>
2021-04-22 17:06:35 +0800

    Updating

--- js/script.js
+++ js/script.js
@@ -1,151 +1,172 @@
-// 'use strict';
-
 const input = document.querySelector('input'),
-      btnAllItems = document.querySelector('#all-items'),
-      btnActiveItems = document.querySelector('#active-items'),
-      btnCompletedItems = document.querySelector('#completed-items');
+      listTodo = document.querySelector('.todo-list__items'),
+      allItemsBtn = document.querySelector('#all-items'),
+      activeItemsBtn = document.querySelector('#active-items'), 
+      completedItemsBtn = document.querySelector('#completed-items'),
+      clearCompletedBtn = document.querySelector('.todo-list__clear-completed'),
+      counterText = document.querySelector('#counter');
 
-// function addNewTodo() {
-
-//     input.addEventListener('keydown', (e) => {
-
-//         let todo = document.createElement('div');
-//         todo.className = 'todo';
-//         todo.innerHTML = `${input.value}`;
-        
-//         let circle = document.createElement('div');
-//         circle.className = 'emptyCircle';
-//         todo.prepend(circle);
-    
-//         let iconCheck = document.createElement('img');
-//         iconCheck.src = "img/icon-check.svg";
-//         iconCheck.alt = "icon-check";
-//         iconCheck.className = 'icon-check hidden';
-//         circle.append(iconCheck);
-    
-//         let img = document.createElement('img');
-//         img.src = "img/icon-cross.svg";
-//         img.alt = 'icon-cross';
-//         img.className = 'close-button hidden';
-//         todo.append(img);
-
-//         if (e.key === "Enter") {
-//             if (input.value == '') {
-//                 alert('Create a new todo (:');
-//             } else {
-//                 document.querySelector('.todo-list__items').append(todo);
-//                 input.value = '';
-//             }
-//         }
-
-//         todo.addEventListener('mouseover', ()=> {
-//             img.classList.remove('hidden');
-//         });
-    
-//         todo.addEventListener('mouseout', ()=> {
-//             img.classList.add('hidden');
-//         });
-    
-//         img.addEventListener('click', (e) => {
-//             e.target.parentElement.remove();
-//         });
-
-        // circle.addEventListener('click', () =>{
-        //     todo.classList.toggle('checked');
-        //     circle.classList.toggle('checked-circle');
-        //     iconCheck.classList.toggle('hidden');
-        // });
-
-//         btnActiveItems.addEventListener('click', () => {
-//             if (!todo.classList.contains('checked')) {
-                
-//             }
-
-
-//         });
-
-//     });
-// }
-
-// addNewTodo();
-
-
+let todos = listTodo.children;
+let counter = 0;
 
 class Todo {
     constructor (title) {
         this.title = title;
-        this.checked = false;
     }
     
     render () {
         const todo = document.createElement('div');
-
+        todo.className = 'todo';
         todo.innerHTML = `
-        <div class="todo">
-            <div class="emptyCircle">
-                <img src="img/icon-check.svg" alt="icon-check" class="icon-check hidden">
-            </div>
-            ${this.title}
+            <div class="emptyCircle"></div>          
+            <div>${this.title}</div>
             <img src="img/icon-cross.svg" alt="icon-cross" class="close-button hidden">
-        </div>`;
+        `;
         
         return todo;
     }
 }
 
-const newTodo1 = new Todo ("Thomas");
-const newTodo2 = new Todo ("Rose");
+//Create a new element-todo.
+function createTodo() {
+    const newDiv = new Todo (input.value);
+    const newTodo = newDiv.render(); 
+    
+    document.querySelector(".todo-list__items").append(newTodo);
 
-let todoArray = [];
+    input.value = '';
+    return newTodo;
+}
 
-todoArray.push(newTodo1);
-todoArray.push(newTodo2);
-
-function addNewTodo() {
-
+//Add new created element to the list if click on "Enter";
+function addTodo() {
     input.addEventListener('keydown', (e) => {
+        if (e.key == "Enter") {
+            if (input.value !== '') {
+                createTodo();
 
-        if (e.key === "Enter") {
-            if (input.value == '') {
-                alert('Create a new todo (:');
+                //Counter
+                counter = counter + 1;
+                counterText.textContent = counter;
             } else {
-                document.querySelector('.todo-list__items').append(todo);
-                    input.value = '';
+                alert('Please write a new todo');
             }
+        }
+    });
+}
+
+addTodo();
+
+//Add hovering to a close button
+
+function toggleCloseButton() {
+    listTodo.addEventListener('mouseover', (e) => {
+        if(e.target.classList.contains('todo')) {
+            e.target.lastElementChild.classList.remove('hidden');
+        }
+    });
+    
+    listTodo.addEventListener('mouseout', (e) => {
+        if(e.target.classList.contains('todo')) {
+            e.target.lastElementChild.classList.add('hidden');
+        }
+    });
+}
+
+toggleCloseButton();
+
+//Toggle to checked classes if click on checkmark 
+
+function checkTodo() {
+    listTodo.addEventListener('click', (e) => {
+        if (e.target.classList.contains('emptyCircle')) {
+            e.target.classList.toggle('checked-circle');
+            e.target.parentElement.classList.toggle('checked');
+
+                //Counter 
+                if (e.target.className == 'emptyCircle checked-circle') {
+                    counter = counter - 1;
+                    counterText.textContent = counter;
+                } else if (e.target.className == 'emptyCircle') {
+                    counter = counter + 1;
+                    counterText.textContent = counter;
+                }
+        }; 
+    });
+}
+
+checkTodo();
+
+//Delete item if click on close button
+
+function deleteTodo() {
+    listTodo.addEventListener('click', (e) => {
+        if (e.target.classList.contains('close-button')) {
+            e.target.parentElement.remove();
+
+            //Counter
+            if (e.target.parentElement.className == 'todo') {
+                counter = counter - 1;
+                counterText.textContent = counter; 
+            }
+        }
+    });
+}
+
+deleteTodo();
+
+//Filter
+
+allItemsBtn.addEventListener('click', () => {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].classList.contains('todo')) {
+            todos[i].style.display="flex";
+        }
     }
 });
 
+function filter(styleFirst,styleSecond) {
+    for (let i = 0; i < todos.length; i++) {
+        if (!todos[i].classList.contains('checked')) {
+            todos[i].style.display = styleFirst;
+        } else if (todos[i].classList.contains('checked')) {
+            todos[i].style.display = styleSecond;
+        }
+    }
+}
 
+activeItemsBtn.addEventListener('click', () => {
+    filter("flex", "none");
+});
 
+completedItemsBtn.addEventListener('click', () => {
+    filter('none', "flex");
+});
 
+//Button "clear completed"
 
+function clearCompleted() {
+    clearCompletedBtn.addEventListener('click', () => {
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].classList.contains('checked')) {
+                todos[i].remove();
+            }
+        }
+    });  
+}
 
+clearCompleted();
 
+//Toggle dark mode
 
+if(!localStorage.theme) localStorage.theme ='lightMode'
+document.body.className = localStorage.theme;
+toggleThemeBtn.firstElementChild.src = document.body.classList.contains('darkTheme') ? 'img/icon-sun.svg' : 'img/icon-moon.svg';
 
+toggleThemeBtn.onclick = () => {
+    document.body.classList.toggle('darkTheme');
+    toggleThemeBtn.firstElementChild.src = document.body.classList.contains('darkTheme') ? 'img/icon-sun.svg' : 'img/icon-moon.svg';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    localStorage.theme = document.body.className || 'lightMode';
+};
 
